ad565b8904c2b232a43fd09bcb3f61521d426823
Haaragard <diogololz159@gmail.com>
2020-09-14 13:17:38 -0300

    Adicionado ações de atualização e conversão de valores

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=";
 
 Future main() async {
   await DotEnv().load('.env');
-
-  print(await _getData());
   runApp(MyApp());
 }
 
@@ -52,6 +50,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _realController = TextEditingController();
+  final TextEditingController _dolarController = TextEditingController();
+  final TextEditingController _euroController = TextEditingController();
+
+  double _real;
+  double _dolar;
+  double _euro;
+
+  void _realChanged(String text) {
+    double real = double.tryParse(text);
+    if (real != null) {
+      _dolarController.text = (real/_dolar).toStringAsFixed(2);
+      _euroController.text = (real/_euro).toStringAsFixed(2);
+    } else _clearFields();
+  }
+
+  void _dolarChanged(String text) {
+    double dolar = double.tryParse(text);
+    if (dolar != null) {
+      _realController.text = (dolar * this._dolar).toStringAsFixed(2);
+      _euroController.text = ((dolar * this._dolar) / this._euro).toStringAsFixed(2);
+    } else _clearFields();
+  }
+
+  void _euroChanged(String text) {
+    double euro = double.tryParse(text);
+    if (euro != null) {
+      _realController.text = (euro * this._euro).toStringAsFixed(2);
+      _dolarController.text = ((euro * this._euro) / this._dolar).toStringAsFixed(2);
+    } else _clearFields();
+  }
+
+  _clearFields() {
+    _realController.text = _dolarController.text = _euroController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,14 +107,39 @@ class _HomeState extends State<Home> {
                 if (snapshot.hasError) {
                   return ErrorLoadingData();
                 } else {
-                  final double dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
-                  final double euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  this._dolar =
+                    snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  this._euro =
+                    snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
-                  return Container(color: Colors.green,);
+                  return _buildConversorBody();
                 }
             }
           }
       ),
+    );
+  }
+
+  Widget _buildConversorBody() {
+    return ListView(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0),
+      children: <Widget>[
+        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber,),
+
+        // Reais
+        buildTextField("Reais", "R\$ ", _realController, _realChanged),
+
+        Divider(),
+
+        // Dolares
+        buildTextField("Dólares", "US\$ ", _dolarController, _dolarChanged),
+
+        Divider(),
+
+        // Euros
+        buildTextField("Euros", "€ ", _euroController, _euroChanged),
+
+      ],
     );
   }
 }
@@ -109,7 +168,17 @@ class ErrorLoadingData extends StatelessWidget {
   }
 }
 
-
-
-
-
+Widget buildTextField(String label, String prefix, TextEditingController controller, Function onChanged) {
+  return TextField(
+    controller: controller,
+    onChanged: onChanged,
+    decoration: InputDecoration(
+      labelText: "$label",
+      labelStyle: TextStyle(color: Colors.amber),
+      border: OutlineInputBorder(),
+      enabledBorder: OutlineInputBorder(),
+      prefixText: "$prefix",
+    ),
+    keyboardType: TextInputType.number,
+  );
+}
